19bd6f20918a30be7da78beaf6a5fdc8b80daceb
sydney miranda <sydmiranda@icloud.com>
2021-01-10 02:23:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,3 +132,60 @@ def num_points_scored(player_name)
 players = game_hash[:home][:players].merge(game_hash[:away][:players])
 players[player_name]
 end
+
+def shoe_size(player_n)
+  game_hash.each do |key, value|
+    value[:players].each do |player|
+      return player[:shoe] if player[:player_name] == player_n
+    end
+  end
+end
+white_check_mark
+eyes
+raised_hands
+
+
+
+
+
+8:56
+def team_colors(team_n)
+  game_hash.each do |key, value|
+    if value[:team_name] == team_n
+      return value[:colors]
+    end
+  end
+end
+def team_names
+  game_hash.map {|key, value| value[:team_name]}
+end
+def player_numbers(team_n)
+  game_hash.each do |key, value|
+    if value[:team_name] == team_n
+      return value[:players].map {|player| player[:number]}
+    end
+  end
+end
+def player_stats (player_n)
+  game_hash.each_key do |ha|
+    game_hash[ha][:players].each_entry do |playerhash|
+      if playerhash[:player_name] == player_n
+        return playerhash
+      end
+    end
+  end
+end
+def big_shoe_rebounds
+  biggest = 0
+  rebounds = 0
+  game_hash.each do |key, value|
+    value[:players].each do |player|
+      size = player[:shoe]
+      if size > biggest
+        biggest = size
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  rebounds
+end
