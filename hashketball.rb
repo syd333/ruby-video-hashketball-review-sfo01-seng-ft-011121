4741d3401e8d52bb783210fa68e091e330a49c66
sydney miranda <sydmiranda@icloud.com>
2021-01-10 02:20:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,5 +129,6 @@ end
 
 # Write code here
 def num_points_scored(player_name)
-game_hash[:home][:players].merge(game_hash[:away][:players])
+players = game_hash[:home][:players].merge(game_hash[:away][:players])
+
 end
